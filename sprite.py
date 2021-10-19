a02fa19cd92d221c86c249e37a86d2bc32d0443a
DancheChoi <49130133+DancheChoi@users.noreply.github.com>
2021-10-19 16:56:44 +0100

    Update sprite.py

--- sprite.py
+++ sprite.py
@@ -1,15 +1,17 @@
-class sprite:                                                 #   Creates class 'sprite'
-    class hitbox:                                             #   Creates class 'hitbox'
-        def ManageCoords(self, operation, x1, y1, x2, y2):    #   Gets operation (write or get), if write, gets x1, y1, x2, y2, if get, retrieves stored coordinates
-            if self.operation == "write":                 				#   If operation is to write, the parameters x1, y1, x2, y2 are stored in the corrisponding variables
+class sprite:                                                   #   Creates class 'sprite'
+    class hitbox:                                               #   Creates class 'hitbox'
+        def ManageCoords(self, operation, x1, y1, x2, y2):      #   Gets operation (write or get), if write, gets x1, y1, x2, y2, if get, retrieves stored coordinates
+            if self.operation == "write":                 	    #   If operation is to write, the parameters x1, y1, x2 and y2 are stored in the corrisponding variables
                 self.x1 = x1
                 self.y1 = y1
                 self.x2 = x2
                 self.y2 = y2
 
-            elif self.operation == "get":
+            elif self.operation == "read":                      #   If operation is to read, the values stored in x1, y1, x2 and y2 are returned.
                 return x1
                 return y1
                 return x2
                 return y2
-         
+            
+            else:                                               #   If anything else is entered as the operation, we return false.
+                return False
