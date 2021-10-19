f435bfe2b0bb99f9c53fd27bcfa999548127885f
DancheChoi <49130133+DancheChoi@users.noreply.github.com>
2021-10-19 17:42:33 +0100

    rename sprite.py to main.py and debugging

---
+++ main.py
@@ -0,0 +1,7 @@
+class sprite:                                                   #   Creates class sprite
+    class hitbox:                                               #   Creates subclass hitbox
+        def Coords(x1, y1, x2, y2):                             # The __init__ function in the hitbox returns a list of entered coordinates.
+            Coords=[x1, y1, x2, y2]
+            return Coords
+        def DetectCollision():
+            pass
